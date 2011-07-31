d191b7140eada2e2842a9810fc60722093f0d1a5
Matthew Liu <matthewliu@gmail.com>
2011-07-31 15:01:55 -0700

    Resetting everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blojobb::Application.config.secret_token = '46f3e667757c0eee18eb91cad0e0bf6913f0058d43e137c468943b3dd15dc4bedd84ff036e222846aa1565720e04660bcff8c081e43c3a0f8866a7290d64aea2'
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe PagesController do
+
+render_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+  end
+
+  describe "Get 'results" do
+    it "should be successful" do
+      get 'results'
+      response.should be_success
+    end
+  end
+
+end
